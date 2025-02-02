edc2616a170e9e37e094a76dbb9d2ff5c13b593e
malexj <alexmaximovj@gmail.com>
2025-02-02 13:56:40 +0200

    Fix tests

--- src/main/java/com/be/stack/game/MainVerticle.java
+++ src/main/java/com/be/stack/game/MainVerticle.java
@@ -3,6 +3,7 @@ package com.be.stack.game;
 import com.be.stack.game.handler.AuthFilterHandler;
 import com.be.stack.game.handler.FailureHandle;
 import com.be.stack.game.handler.UserHandler;
+
 import io.github.cdimascio.dotenv.Dotenv;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
@@ -16,19 +17,16 @@ public class MainVerticle extends AbstractVerticle {
 
   private static final Logger LOG = Logger.getLogger(MainVerticle.class.getName());
 
-  private final Integer port =
-      Optional.of(Dotenv.load().get("SERVER_PORT", "8080"))
-          .map(Integer::parseInt)
-          .orElseThrow(() -> new RuntimeException("Server port not set"));
+  private final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+  private final Integer port = Optional.of(dotenv.get("SERVER_PORT", "8080")).map(Integer::parseInt)
+    .orElseThrow(() -> new RuntimeException("Server port not set"));
 
   private MongoClient initMongoClient() {
-    Dotenv dotenv = Dotenv.load();
     String mongoUri = dotenv.get("MONGO_URI", "mongodb://localhost:27017");
     String mongoDb = dotenv.get("MONGO_DB", "mongodb");
-    return MongoClient.createShared(
-        vertx,
-        new JsonObject().put("connection_string", mongoUri).put("db_name", mongoDb),
-        "BE_STACK_POOL");
+    return MongoClient.createShared(vertx, new JsonObject().put("connection_string", mongoUri).put("db_name", mongoDb),
+      "BE_STACK_POOL");
   }
 
   @Override
@@ -40,27 +38,19 @@ public class MainVerticle extends AbstractVerticle {
     var userHandler = new UserHandler(mongoClient);
     var failureHandle = new FailureHandle();
     var router = Router.router(vertx);
-    router
-        .get("/api/user")
-        .handler(authFilterHandler::handle)
-        .handler(userHandler::findAllUserInfoByUserId)
-        .failureHandler(failureHandle::handle);
+    router.get("/api/user").handler(authFilterHandler::handle).handler(userHandler::findAllUserInfoByUserId)
+      .failureHandler(failureHandle::handle);
     // ************ router ***********************
 
-    vertx
-        .createHttpServer()
-        .requestHandler(router)
-        .listen(
-            port,
-            http -> {
-              if (http.succeeded()) {
-                startPromise.complete();
-                LOG.info("Server started on port %s".formatted(port));
-              } else {
-                Throwable error = http.cause();
-                LOG.severe(error.getLocalizedMessage());
-                startPromise.fail(error);
-              }
-            });
+    vertx.createHttpServer().requestHandler(router).listen(port, http -> {
+      if (http.succeeded()) {
+        startPromise.complete();
+        LOG.info("Server started on port %s".formatted(port));
+      } else {
+        Throwable error = http.cause();
+        LOG.severe(error.getLocalizedMessage());
+        startPromise.fail(error);
+      }
+    });
   }
 }
--- src/test/java/com/be/stack/game/TestMainVerticle.java
+++ src/test/java/com/be/stack/game/TestMainVerticle.java
@@ -1,20 +1,28 @@
 package com.be.stack.game;
 
-import io.vertx.core.Vertx;
-import io.vertx.junit5.VertxExtension;
-import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+
 @ExtendWith(VertxExtension.class)
 class TestMainVerticle {
 
+
+  @BeforeAll
+  static void setup() {
+    Dotenv.configure().ignoreIfMissing().load();
+  }
+
+
   @BeforeEach
   void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
-    vertx
-        .deployVerticle(new MainVerticle())
-        .onComplete(testContext.succeeding(id -> testContext.completeNow()));
+    vertx.deployVerticle(new MainVerticle()).onComplete(testContext.succeeding(id -> testContext.completeNow()));
   }
 
   @Test
